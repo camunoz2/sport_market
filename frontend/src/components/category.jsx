c4669282525901701ca88055a075c3f97afa9014
jesus bloise <jesusbloise@gmail.com>
2025-01-29 21:29:35 -0300

    limpiando el codigo anterior

--- frontend/src/components/category.jsx
+++ frontend/src/components/category.jsx
@@ -18,14 +18,3 @@ const Category = ({ imageSrc, title }) => {
 
 export default Category;
 
-// const Category = () => {
-//   return (
-//     <div className="border-1 border-black rounded-sm flex items-center justify-center h-64">
-//       <div className="rounded-sm border-1 border-black text-center uppercase">
-//         <h2>Categoria</h2>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Category;
