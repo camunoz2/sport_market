b2c52fec8db845c0f64dd114c47998c530d1dc4f
Marco Navarrete <marconav2000@gmail.com>
2025-01-30 21:59:25 -0300

    se agrego el registro de usuario Se agrego archivo imagen de registro Se agrego ruta en  archivo Register.jsx

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "../pages/Home.jsx";
 import ProductDetail from "../pages/ProductDetail.jsx";
 import Login from "../pages/Login.jsx";
+import Register from "../pages/Register.jsx";
 
 const AppRouter = () => {
   return (
@@ -9,6 +10,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/Registro" element={<Register />} />
         <Route path="/productos/:id" element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
