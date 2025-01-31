fed679a5fd379033aeff64cbeb7158e476bf107d
Cristian M <camunoz2@gmail.com>
2025-01-31 11:19:57 -0300

    el logo lleva al home

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -2,9 +2,9 @@ const Header = () => {
   return (
     <header className="p-4 bg-gray-800 shadow-md">
       <div className="container mx-auto flex items-center justify-between ">
-        <div className="flex items-center">
+        <a href="/" className="flex items-center">
           <img src="/logo.svg" alt="Logo" className="h-10" />
-        </div>
+        </a>
 
         <div className="flex items-center">
           <div className="flex flex-col">
