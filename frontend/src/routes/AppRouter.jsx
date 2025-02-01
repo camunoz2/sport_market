b869c2caefcabe92268a4b30864d97a4c50a0a44
jesus bloise <jesusbloise@gmail.com>
2025-02-01 19:31:05 -0300

    se crea una pagina de productos solo

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -5,6 +5,7 @@ import Login from "../pages/Login.jsx";
 import Register from "../pages/Register.jsx";
 import BaseLayout from "../layouts/base-layout.jsx";
 import CreatePost from "../pages/CreatePost.jsx";
+import ProductsPage from "../pages/ProductsPage";
 
 const AppRouter = () => {
   return (
@@ -14,6 +15,7 @@ const AppRouter = () => {
         <Route element={<BaseLayout />}>
           <Route path="/login" element={<Login />} />
           <Route path="/registro" element={<Register />} />
+          <Route path="/productos" element={<ProductsPage />} />
           <Route path="/productos/:id" element={<ProductDetail />} />
           <Route path="/publicar" element={<CreatePost />} />
         </Route>
