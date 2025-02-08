7b47fede8894d0a62b56816323a56f650f550187
Marco Navarrete <marconav2000@gmail.com>
2025-02-08 18:17:55 -0300

    Creación de archivo .env en backend estructura pool para manejo de información llenado de archivo backend/src/config/db.js

---
+++ backend/src/config/db.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const { Pool } = require('pg')
+
+const pool = new Pool(
+    {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        port: 5432,
+        allowExitOnIdle: true
+    }
+)
+
+module.exports = pool;
