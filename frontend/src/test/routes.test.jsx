db65acc1f72c08ea6bf6c2001ee9b0ef39ba9e09
Cristian M <camunoz2@gmail.com>
2025-02-08 13:38:22 -0300

    agrege el testeo de las categorias

--- frontend/src/test/routes.test.jsx
+++ frontend/src/test/routes.test.jsx
@@ -1,13 +1,20 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
 import { MemoryRouter, Routes, Route } from "react-router";
-import { describe, it, expect } from "vitest";
+import { describe, it, afterEach, expect } from "vitest";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
-
+import ProductsPage from "../pages/ProductsPage";
 import { CartProvider } from "../context/CartContext";
 import { AuthProvider } from "../context/AuthContext";
+import { generateCategories } from "../utils/generateCategories";
+
+const categories = generateCategories();
 
 describe("AppRouter", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
   it("renders Home page on default route", () => {
     render(
       <AuthProvider>
@@ -20,7 +27,7 @@ describe("AppRouter", () => {
         </CartProvider>
       </AuthProvider>
     );
-    expect(screen.getByText("Categorías")).not.toBeNull();
+    expect(screen.getByText(/Categorías/i)).not.toBeNull();
   });
 
   it("renders Login page on /login route", () => {
@@ -36,5 +43,24 @@ describe("AppRouter", () => {
       </AuthProvider>
     );
     expect(screen.getByText(/Bienvenido/i)).not.toBeNull();
+  });
+
+  it("renders Products page with category on /productos route", () => {
+    render(
+      <AuthProvider>
+        <CartProvider>
+          <MemoryRouter
+            initialEntries={[`/productos?category=${categories[0]}`]}
+          >
+            <Routes>
+              <Route path="/productos" element={<ProductsPage />} />
+            </Routes>
+          </MemoryRouter>
+        </CartProvider>
+      </AuthProvider>
+    );
+    expect(
+      screen.getByRole("heading", { level: 2, name: categories[0] })
+    ).not.toBeNull();
   });
 });
