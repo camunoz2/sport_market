2acc12ffaaf79a24e877316ea0a6712c75428e99
Cristian M <camunoz2@gmail.com>
2025-02-18 17:30:23 -0300

    fix: netlify ping function

--- frontend/netlify/functions/ping-db.js
+++ frontend/netlify/functions/ping-db.js
@@ -6,19 +6,19 @@ export default async function (event, context) {
     if (!response.ok) {
       throw new Error(`Request failed with status: ${response.status}`);
     }
-    // Optionally, process the response:
+
     const result = await response.text();
     console.log("Successfully pinged API:", result);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "API is awake", result }),
-    };
+    return new Response(JSON.stringify({ message: "API is awake", result }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
   } catch (error) {
     console.error("Error pinging API:", error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.toString() }),
-    };
+    return new Response(JSON.stringify({ error: error.toString() }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
   }
 }
